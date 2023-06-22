37298dd99bbe622384a04153aefb40352a572556
helenmao <ym102@wellesley.edu>
2023-06-22 13:47:16 -0400

    create standardized tables in database

--- all_connected/data/create_tables.sql
+++ all_connected/data/create_tables.sql
@@ -1,42 +1,47 @@
 USE snapngo_db;
 
+DROP TABLE IF EXISTS `assignments`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `tasks`;
 
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(50),
     `name` VARCHAR(50),
     compensation DECIMAL(4,2) DEFAULT 0,
-    reliability DECIMAL(4,2),
+    reliability DECIMAL(4,2) DEFAULT NULL,
     PRIMARY KEY (id)
 )
 ENGINE = InnoDB;
 
+
 CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT,
-    location VARCHAR(100),
+    `location` VARCHAR(100),
     `description` VARCHAR(100),
+    start_time DATETIME,
     deadline DATETIME,
-    `time_window` INT(3),
-    compensation DECIMAL(4,2),
+    time_window INT(3),
+    compensation DECIMAL(4,2) DEFAULT 0,
     expired BOOLEAN,
     PRIMARY KEY (id)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS assignments (
-    id INT,
-    taskID INT,
-    userID VARCHAR(15),
-    recommendTime DATETIME,
-    img BLOB,
-    submissionTime DATETIME,
-    status ENUM('not assigned','accepted','rejected','pending'),
+    id INT AUTO_INCREMENT,
+    task_id INT,
+    user_id VARCHAR(15),
+    recommend_time DATETIME,
+    img varchar(100),
+    submission_time DATETIME,
+    `status` ENUM('not assigned','accepted','rejected','pending') DEFAULT 'not assigned',
     PRIMARY KEY (id),
-    FOREIGN KEY (taskID) REFERENCES tasks(id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    FOREIGN KEY (userID) REFERENCES users(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE
+        ON DELETE SET NULL
 )
 ENGINE = InnoDB;
 
